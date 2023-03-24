8f00bd5a3a31fb584b64827cdb39d9010094081a
Sssethu26 <sesethugubhethuka@gmail.com>
2023-03-24 13:40:59 +0000

    Task eleven

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,9 +10,7 @@ void print_diagonal(int n)
 		_putchar('\n');
 	} else
 	{
-		int i, j;
-		
-		for (i = 0; i < n; i++)
+		int i, j;															for (i = 0; i < n; i++)
 		{
 			for (j = 0; j = n; j++)
 			{
