cf45160e3d484f86d93c3dffb8f9fb7d60919ff6
Sssethu26 <sesethugubhethuka@gmail.com>
2023-03-24 23:09:08 +0000

    The numbers speak for themselves mandatory , clear

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers since 0 p to 9
- * Return: The numbers since 0 up to 9
+ * print_numbers - Function that prints numbers
+ * since 0 p to 9
+ * Return: 0
  */
+
 void print_numbers(void)
-
 {
-	char c;
+	char c = '0';
 
-	for (c = 0; c <= 9; c++)
+	while (c <= '9')
 	{
-		putchar(c);
+		_putchar(c);
+		c++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
