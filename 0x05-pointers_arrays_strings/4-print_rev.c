d48b8ad63f7b11c36eef3a670e17688939f36aaa
Sssethu26 <sesethugubhethuka@gmail.com>
2023-03-27 08:28:32 +0000

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_rev - function that prints a string
+ *@s: The string that print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int = 0;
+
+	while (s(c) != '\0')
+	{
+	c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+	_putchar(s[c]);
+	}
+	_putchar('\n');
+}
