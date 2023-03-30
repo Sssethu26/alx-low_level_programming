a092529d2e59f552af553a7cbec2ad00d5f8105e
Sssethu26 <sesethugubhethuka@gmail.com>
2023-03-30 12:44:06 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-	while ((str[index] >= 'a' && str[index] <= 'z'))
+	while (!(str[index] >= 'A' && str[index] <= 'Z'))
 	index++;
 	if (str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
