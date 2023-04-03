cb69dcf9b8aab9dd5927ae678746eca695b73706
Sssethu26 <sesethugubhethuka@gmail.com>
2023-04-03 13:02:34 +0000

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,15 +10,16 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
+	int k;
 
-	for (i = 0; s[i] != '\0'; n++)
+	while (*s)
 	{
-		for (n = 0; accept[n] != '\0'; n++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (s[i] == accept[n])
-				return (s + i);
+			if (*s == accept[k])
+				return (s);
 		}
+	s++;
 	}
 	return (0);
 }
