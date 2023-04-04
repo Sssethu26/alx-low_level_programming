81099caa452ddcd21d7027abebdaf59280e3a1b5
Sssethu26 <sesethugubhethuka@gmail.com>
2023-04-04 20:17:04 +0000

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,23 +3,23 @@
 /**
  * leet - encode into 1337speak
  * @n: input value
- * Return: str value
+ * Return: n value
  *
  */
-char *leet(char *str)
+char *leet(char *n)
 {
 	int i, j;
 
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; a[j] != '\0'; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (str[i] == a[j])
-				str[i] = b[j];
+			if (n[i] == s1[j])
+				n[i] = s2[j];
 		}
 	}
-	return (str);
+	return (n);
 }
