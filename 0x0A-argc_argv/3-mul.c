77eaf18288f8ac9fccb1c1a6ce314244a2b705a1
Sssethu26 <sesethugubhethuka@gmail.com>
2023-04-11 02:14:05 +0000

    Task 03

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Prints the multiplication of two args numbers.
@@ -14,8 +15,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		n1 = _atoi(argv[1]);
-		n2 = _atoi(argv[2]);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
 		printf("%d\n", n1 * n2);
 	}
 	else
